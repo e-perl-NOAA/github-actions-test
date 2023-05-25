c12e0c87392bfd33c0790347e0e4c53154ff33a6
Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>
2023-05-25 14:39:54 +0000

    get ss3 zip

--- .devcontainer/setup-admb.sh
+++ .devcontainer/setup-admb.sh
@@ -3,8 +3,8 @@ sudo unzip admb-13.1-linux.zip -d /usr/local/bin
 sudo chmod 755 /usr/local/bin/admb-13.1/bin/admb
 export PATH=$PATH:/usr/local/bin/admb-13.1/bin
 
-rm -rf SS330
-rm -rf ss_osx.tar
-mkdir SS330
-/bin/bash ./Make_SS_330_new.sh -b SS330 -p
-/bin/bash ./Make_SS_330_new.sh -b SS330 -o -p
+
+wget https://github.com/nmfs-stock-synthesis/stock-synthesis/refs/tags/v3.30.21.zip
+sudo unzip v3.30.21.zip -d /usr/local/bin
+sudo chmod 755 /usr/local/bin/stock-synthesis-3.30.21
+/bin/bash ./Make_SS_330_new.sh -b
