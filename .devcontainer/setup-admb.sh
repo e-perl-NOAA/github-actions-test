f7e41c77d7ce4be118c4e8b4b1a77d0023e3b291
Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>
2023-05-25 10:01:04 -0400

    Update setup-admb.sh

--- .devcontainer/setup-admb.sh
+++ .devcontainer/setup-admb.sh
@@ -1,2 +1,10 @@
 wget https://github.com/admb-project/admb/releases/download/admb-13.1/admb-13.1-linux.zip
 sudo unzip admb-13.1-linux.zip -d /usr/local/bin
+sudo chmod 755 /usr/local/bin/admb-13.1/bin/admb
+export PATH=$PATH:/usr/local/bin/admb-13.1/bin
+
+rm -rf SS330
+rm -rf ss_osx.tar
+mkdir SS330
+/bin/bash ./Make_SS_330_new.sh -b SS330 -p
+/bin/bash ./Make_SS_330_new.sh -b SS330 -o -p
