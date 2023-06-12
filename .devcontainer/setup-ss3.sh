ea126f69e0b95ebfeff2b2ef52eec7377803f9e5
Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>
2023-06-12 17:45:14 +0000

    update scripts

--- .devcontainer/setup-ss3.sh
+++ .devcontainer/setup-ss3.sh
@@ -1,11 +1,17 @@
-wget -O ss https://github.com/nmfs-stock-synthesis/stock-synthesis/releases/latest/download/ss_linux
-sudo chmod a+x ss
+sudo apt-get update
+wget https://github.com/admb-project/admb/releases/download/admb-13.1/admb-13.1-linux.zip
+sudo unzip admb-13.1-linux.zip && mv admb-13.1 admb
+sudo rm admb-13.1-linux.zip
+sudo chmod 777 admb
+sudo cp -Rvf admb /usr/local/admb
+export PATH=$PATH:/usr/local/admb/bin
 
-wget https://github.com/nmfs-stock-synthesis/user-examples/archive/refs/heads/main.zip
-sudo unzip main.zip
-sudo chmod a+x user-examples-main
-sudo mv user-examples-main/model_files model_files
-sudo chmod 777 model_files
-sudo rm -r -f user-examples-main
-sudo rm -r -f main.zip
-sudo cp ss model_files/simple
+cd /workflows/github-actions-test
+wget https://github.com/nmfs-stock-synthesis/stock-synthesis/archive/main.zip
+sudo unzip main.zip -d /usr/local/bin
+sudo chmod 777 /usr/local/bin/stock-synthesis-main
+cd /usr/local/bin/stock-synthesis-main
+sudo make
+mv /usr/local/bin/stock-synthesis-main/ss /workspaces/github-actions-test/ss
+
+echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))' | sudo sh -c 'cat - >>\"${R_HOME}/etc/Rprofile.site\"'
