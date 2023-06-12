6f9dccb28074013767741e7cdb4520867b0ff774
Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>
2023-06-12 09:03:10 -0400

    Update setup-admb.sh

--- .devcontainer/setup-admb.sh
+++ .devcontainer/setup-admb.sh
@@ -15,16 +15,4 @@ sudo unzip v3.30.21.zip -d /usr/local/bin
 sudo chmod 777 /usr/local/bin/stock-synthesis-3.30.21
 /bin/bash ./Make_SS_330_new.sh -b
 
-wget -O ss https://github.com/nmfs-stock-synthesis/stock-synthesis/releases/latest/download/ss_linux
-sudo chmod a+x ss
-
-wget https://github.com/nmfs-stock-synthesis/user-examples/archive/refs/heads/main.zip
-sudo unzip main.zip
-sudo chmod a+x user-examples-main
-sudo mv user-examples-main/model_files model_files
-sudo chmod 777 model_files
-sudo rm -r -f user-examples-main
-sudo rm -r -f main.zip
-sudo cp ss model_files/simple
-
 echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))' | sudo sh -c 'cat - >>\"${R_HOME}/etc/Rprofile.site\"'
