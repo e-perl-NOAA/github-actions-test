99c7063994f5fd67ec0a23baf775c95c8244e4cd
Elizabeth Gugliotti <elizabeth.gugliotti@noaa.gov>
2023-06-12 11:40:08 -0400

    Update setup-admb.sh

--- .devcontainer/setup-admb.sh
+++ .devcontainer/setup-admb.sh
@@ -4,14 +4,16 @@ sudo apt-get -y install bison
 wget https://github.com/admb-project/admb/releases/download/admb-13.1/admb-13.1-src.zip
 sudo unzip admb-13.1-src.zip && mv admb-13.1 admb
 sudo rm admb-13.1-src.zip
+sudo chmod 777 admb
 cd admb
-sudo make install
-sudo ln -sf /usr/local/admb/admb /usr/local/bin/admb
+sudo make
+sudo cp -Rvf build/admb /usr/local/admb
 export PATH=$PATH:/usr/local/admb/admb
 cd examples/admb/simple
 sudo admb simple.tpl
 sudo ./simple
 
+cd /workflows/github-actions-test
 wget https://github.com/nmfs-stock-synthesis/stock-synthesis/archive/refs/heads/main.zip
 sudo unzip main.zip -d /usr/local/bin
 sudo chmod 777 /usr/local/bin/stock-synthesis-main
